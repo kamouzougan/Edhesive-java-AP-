b54218c13c958c1a06aac7f4ec1f03decb8dbe3a
kangni Amouzougan <kamouzougan@gmail.com>
2020-04-25 06:58:34 -0400

    Create Lesson_2_Activity_One.java

---
+++ Lesson_2_Activity_One.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.Scanner;
+
+class Lesson_2_Activity_One
+{
+ public static void main (String str[]) throws IOException 
+ {
+ //Print the shape using \\ to print a single \.
+ System.out.println("/\\");
+ System.out.println("\\/");
+
+ }
+}
